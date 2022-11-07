f55e7829180106749c2206fc97f37b377748f5d5
anelv <anvi8741@colorado.edu>
2022-11-07 16:07:34 -0700

    assignment 11

--- assignments/assignment11/assignment11.js
+++ assignments/assignment11/assignment11.js
@@ -3,7 +3,7 @@ quoteButton.addEventListener('click', getQuote);
 
 const doggo = document.querySelector('img');
 
-const endpoint = 'http://www.boredapi.com/api/activity/';
+const endpoint = 'http://www.boredapi.com/api/activity';
 const endpoint2 = 'https://dog.ceo/api/breeds/image/random'
 
 
