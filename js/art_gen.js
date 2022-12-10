a3157fc7a868281fc2c9ef6954f937298968e6b6
anelv <anvi8741@colorado.edu>
2022-12-10 14:39:36 -0700

    updated nav bar js
    redid some css for the about sections, trying to get the art generator to display: grid instead of display:flex when under 480px screen width

---
+++ js/art_gen.js
@@ -0,0 +1,168 @@
+//Initial References
+let container = document.querySelector(".container");
+let gridButton = document.getElementById("submit-grid");
+let clearGridButton = document.getElementById("clear-grid");
+let gridWidth = document.getElementById("width-range");
+let gridHeight = document.getElementById("height-range");
+let colorButton = document.getElementById("color-input");
+let eraseBtn = document.getElementById("erase-btn");
+let paintBtn = document.getElementById("paint-btn");
+let widthValue = document.getElementById("width-value");
+let heightValue = document.getElementById("height-value");
+
+//Events Objects
+let events = {
+    mouse:{
+        down: "mousedown",
+        move: "mousemove",
+        up: "mouseup",
+    },
+    touch:{
+        down: "touchstart",
+        move: "touchmove",
+        up: "touchend",
+    },
+};
+
+let deviceType = "";
+
+//Initially draw and erase would be false
+let draw = false;
+let erase = false;
+
+//Function to detect touch device
+const isTouchDevice = () => {
+    try{
+        //we try to create TouchEvent(it would fail for desktops and throw error)
+        document.createEvent("TouchEvent")
+        deviceType = "touch";
+        return true;
+    }
+    catch(e){
+        deviceType = "mouse";
+        return false;
+    }
+};
+
+isTouchDevice();
+
+
+//Function to create grid
+
+gridButton.addEventListener("click", () => {
+    //initially clear the grid (old grids cleared)
+    container.innerHTML = "";
+    //count variable for generating unique ids
+    let count = 0;
+    //loop for creating rows
+    for (let i = 0; i < gridHeight.value; i++) { 
+        //incrementing count by 2
+        count += 2;
+        //create row div
+        let div = document.createElement("div");
+        div.classList.add("gridRow");
+        //create colums
+        for (let j = 0; j < gridWidth.value; j++) {
+            count += 2;
+            let col = document.createElement("div");
+            col.classList.add("gridCol");
+            //we need unique ids for all the colums (for touch screen specifically)
+            col.setAttribute("id", `gridCol${count}`);
+            
+            /*for eg if deviceType = "mouse"
+            the statement for the event would be events[mouse].down which equals to mousedown
+            if deviceType="touch"
+            the statement for event would be events[touch].down which equals to touchstart
+            */
+
+            col.addEventListener(events[deviceType].down, () => {
+                //user starts to draw
+                draw = true;
+                /*if erase = true then background = transparent 
+                else color button value */
+                if(erase){
+                    col.style.backgroundColor = "transparent";
+                }
+                else{
+                    col.style.backgroundColor = colorButton.value;
+                }
+            });
+
+            col.addEventListener(events[deviceType].move, (e) => 
+            {
+                //elementFromPoint returns the element at x,y position of mouse
+                let elementId = document.elementFromPoint(
+                    !isTouchDevice() ? e.clientX : e.touches[0].clientX,
+                    !isTouchDevice() ? e.clientY : e.touches[0].clientY
+                ).id;
+                //checker
+                checker(elementId);
+            });
+            //stop drawing
+            col.addEventListener(events[deviceType].up, () => {
+                draw = false;
+            });
+            //append colums
+            div.appendChild(col);
+        }
+        //append grid to container
+        container.appendChild(div);
+    }
+});
+function checker(elementId){
+    let gridColumns = document.querySelectorAll(".gridCol");
+    //loop through all boxes
+    gridColumns.forEach((element) => {
+        //if id matches then color
+        if (elementId == element.id) {
+            if(draw && !erase) {
+                element.style.backgroundColor = colorButton.value;
+            } else if(draw && erase) {
+                element.style.backgroundColor = "transparent";
+            }
+        }
+    });
+}
+
+//adding event listeners to clear grid, erase, and paint buttons
+
+//Clear Grid
+clearGridButton.addEventListener("click", () => {
+    container.innerHTML = "";
+});
+
+//Erase Button
+eraseBtn.addEventListener("click", () => {
+    erase = true;
+});
+
+//Paint Button
+paintBtn.addEventListener("click", () => {
+    erase = false;
+});
+
+//Displaying grid width 
+gridWidth.addEventListener("input", () => {
+    widthValue.innerHTML = gridWidth.value < 9 ? `0${gridWidth.value}`:gridWidth.value;
+});
+//Displaying grid height
+gridHeight.addEventListener("input", () => {
+    heightValue.innerHTML = gridHeight.value < 9 ? `0${gridHeight.value}`:gridHeight.value;
+});
+
+
+window.onload = () => {
+    gridWidth.value = 0;
+    gridHeight.value = 0;
+};
+
+//CODE FOR NAVBAR
+/* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
+function myFunction() {
+    var x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
+      x.className += " responsive";
+    } else {
+      x.className = "topnav";
+    }
+  }
